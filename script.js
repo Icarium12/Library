1ecf3acf734a68fded3dc323e06c8c64a092c685
Praise Mogaji <praisemogaji@gmail.com>
2025-10-24 19:35:09 +0100

    Added book array to script.js
    Created a book object for each book and created a
    function that adds each book object to the array

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const myLibrary = [];
 
+function Book() {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+
+    this.title = title;
+    this.author = author;
+    this.numOfPages = numOfPages;
+    this.readStatus = readStatus;
+    this.id = crypto.randomUUID();
+    this.info = function() {
+        return (this.title + " by "  + this.author + " with " + this.numOfPages + " pages, "  + this.readStatus);
+    }
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
