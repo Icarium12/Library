56ed7f23b26e57015f89923b43ec87d5b4c3acf8
Praise Mogaji <praisemogaji@gmail.com>
2025-10-24 21:56:30 +0100

    Added the content for the library on the page
    Created the container for the library display in
    index.html
    Created function to add book info into container
    for library in script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const myLibrary = [];
+const libraryContainer = document.querySelector(".libraryContainer");
 
-function Book() {
+
+function Book(title, author, numOfPages, readStatus) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
@@ -15,6 +17,50 @@ function Book() {
     }
 }
 
+const book1 = new Book("The way of kings", "Brandon Sanderson", 1000, "read");
+console.log(book1.title);
+
+addBookToLibrary(book1);
+console.log(myLibrary);
+
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
+
+function displayBook(array, libraryContainer) {
+    for(let i = 0; i <= array.length; i++) {
+        const bookCard = document.createElement('div');
+        bookCard.className = 'bookcard';
+        libraryContainer.appendChild(bookCard);
+
+        const book = document.createElement('div');
+        book.className = 'book';
+
+        const title = document.createElement('div');
+        title.textContent = array[i].title;
+        book.appendChild(title);
+
+        const author = document.createElement('div');
+        author.textContent = array[i].author
+        book.appendChild(author);
+
+        const pages = document.createElement('div');
+        pages.textContent = array[i].numOfPages;
+        book.appendChild(pages);
+
+        bookCard.appendChild(book);
+
+        const id = document.createElement('div');
+        id.textContent = array[i].id;
+        bookCard.appendChild(id);
+
+        const status = document.createElement('div');
+        status.textContent = array[i].readStatus;
+        bookCard.appendChild(status);
+    }
+}
+
+displayBook(myLibrary, libraryContainer);
+
+
+
